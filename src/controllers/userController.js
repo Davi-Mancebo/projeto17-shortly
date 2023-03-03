3e53b052f6319b4df640cc59334d5f5d26717549
Davi-Mancebo <davimancebo@gmail.com>
2023-03-03 11:24:50 -0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,64 +4,66 @@ import dayjs from "dayjs";
 import { validateUserRegister } from "../middleware/schemas/userRegisterSchema.js";
 import { validateUserLogin } from "../middleware/schemas/userLoginSchema.js";
 import bcrypt from "bcrypt";
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 
 export const signUp = async (req, res) => {
+  const { name, email, password, error } = await validateUserRegister(req.body);
+
   try {
-    const { name, email, password, error} = await validateUserRegister(req.body);
-    if (error) return res.sendStatus(422).send(error);
+    if (error) {
+      return res.sendStatus(422);
+    }
     const emailExist = await db.query(
-      "SELECT id FROM users WHERE 'email' = $1",
+      "SELECT * FROM users WHERE email = $1",
       [email]
     );
     const hashPassword = await bcrypt.hash(password, 8);
-
-    if(emailExist.rows.length > 0) return res.sendStatus(409)
+    console.log(email);
+    if (emailExist.rows.length > 0) return res.sendStatus(409);
 
     await db.query(
-      'INSERT INTO "users" (name, email, password, createdAt) VALUES ($1, $2, $3, $4)',
-      [name, email, hashPassword, Date.now()]
-    )
-    return res.sendStatus(201)
+      'INSERT INTO "users" (name, email, password) VALUES ($1, $2, $3)',
+      [name, email, hashPassword]
+    );
+    return res.sendStatus(201);
   } catch (error) {
     return res.status(500).send(error.message);
   }
 };
 export const signIn = async (req, res) => {
- try{
-    const { email, password, error } = validateUserLogin(req.body)
-    const token = uuid()
-    if(error) res.sendStatus(422).send(error)
-    const userExist = await db.query(
-      USERS + " WHERE email = $1",
-      [email]
-    )
-    if(userExist.rows.length > 0){
+  try {
+    const { email, password, error } = validateUserLogin(req.body);
+    const token = uuid();
+    if (error) res.sendStatus(422).send(error);
+    const userExist = await db.query(USERS + " WHERE email = $1", [email]);
+    if (userExist.rows.length > 0) {
       const comparation = await bcrypt.compare(
         password,
         userExist.rows[0].password
       );
-      if(comparation){
+      if (comparation) {
         const loggedIn = await db.query(
-            SESSIONS + " WHERE userId = $1",
-            [userExist.rows[0].id]
-          )
-        if(loggedIn.rows.length > 0){
-          await db.query(
-            "DELETE FROM sessions WHERE userId = $1",
-            [userExist.rows[0].id]
-            )
+          'SELECT id FROM sessions WHERE "userId" = $1',
+          [userExist.rows[0].id]
+        );
+        if (loggedIn.rows.length > 0) {
+          console.log(loggedIn.rows)
+          await db.query("DELETE FROM sessions WHERE id = $1", [
+            loggedIn.rows[0].id,
+          ]);
         }
-        await db.query(
-          'INSERT INTO sessions (userId, token, createdAt) VALUES ($1, $2, $3)',
-          [userExist.rows[0].id, token, Date.now()]
-          )
-        return res.sendStatus(201).send(token)
+        await db.query('INSERT INTO sessions ("userId", "token") VALUES ($1, $2)', [
+          userExist.rows[0].id,
+          token,
+        ]);
+        return res.status(201).send({
+          token: token
+        });
       }
-      return res.sendStatus(401)
-    }else return res.sendStatus(401)
+      return res.sendStatus(401);
+    } else return res.sendStatus(401);
   } catch (error) {
-    console.log("erro");
-    return res.status(500).send(error.message);
+    console.log(error)
+    return res.sendStatus(500);
   }
 };
