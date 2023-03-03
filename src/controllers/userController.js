b5065b1bb386e656378ec54342214730b3f55e6c
Davi-Mancebo <davimancebo@gmail.com>
2023-03-03 12:53:44 -0300

    end of getting user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,10 +13,9 @@ export const signUp = async (req, res) => {
     if (error) {
       return res.sendStatus(422);
     }
-    const emailExist = await db.query(
-      "SELECT * FROM users WHERE email = $1",
-      [email]
-    );
+    const emailExist = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
     const hashPassword = await bcrypt.hash(password, 8);
     console.log(email);
     if (emailExist.rows.length > 0) return res.sendStatus(409);
@@ -47,26 +46,54 @@ export const signIn = async (req, res) => {
           [userExist.rows[0].id]
         );
         if (loggedIn.rows.length > 0) {
-          console.log(loggedIn.rows)
+          console.log(loggedIn.rows);
           await db.query("DELETE FROM sessions WHERE id = $1", [
             loggedIn.rows[0].id,
           ]);
         }
-        await db.query('INSERT INTO sessions ("userId", "token") VALUES ($1, $2)', [
-          userExist.rows[0].id,
-          token,
-        ]);
+        await db.query(
+          'INSERT INTO sessions ("userId", "token") VALUES ($1, $2)',
+          [userExist.rows[0].id, token]
+        );
         return res.status(200).send({
-          token: token
+          token: token,
         });
       }
       return res.sendStatus(401);
     } else return res.sendStatus(401);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.sendStatus(500);
   }
 };
 export const getMe = async (req, res) => {
-  
-}
+  const user = res.locals.auth;
+  try {
+    const userObject = await db.query(
+      `
+      SELECT USERS.ID AS "id", USERS.NAME AS "name",
+      SUM("visits") AS "visitCount",
+      ARRAY
+      (SELECT JSON_BUILD_OBJECT('id',
+      links.ID,
+      'shortUrl',
+      links."shortUrl",
+      'url',
+      links.URL,
+      'visitCount',
+      links."visits")
+                FROM links
+                JOIN USERS ON links."userId" = USERS.ID
+                WHERE "userId" = $1) AS "shortenedlinks"
+    FROM links
+    JOIN USERS ON links."userId" = USERS.ID
+    WHERE "userId" = $2
+    GROUP BY USERS.ID;
+    `,
+      [user.id, user.id]
+    );
+    return res.send(userObject.rows[0]);
+  } catch (errr) {
+    return res.status(500).send(error);
+  }
+};
